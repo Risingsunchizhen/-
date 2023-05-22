4445bfa940692220a0e6116ec518055cf9337aa8
Risingsunchizhen <130264267+Risingsunchizhen@users.noreply.github.com>
2023-05-22 18:05:55 +0800

    Update 风险评价模型.py

--- 风险评价模型.py
+++ 风险评价模型.py
@@ -6,8 +6,7 @@ warnings.filterwarnings('ignore')
 import backtrader as bt
 import datetime
 import numpy as np
-from scipy.optimize import minimize
-import streamlit as st
+import scipy
 import streamlit as st
 import datetime
 import tushare as ts
@@ -19,15 +18,11 @@ import backtrader as bt
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import font_manager
 import matplotlib.dates as mdates
 import matplotlib.ticker as mtick
 import matplotlib.patches as mpatches
 plt.rcParams['xtick.direction']='in' #####刻度向内
 plt.rcParams['ytick.direction']='out'
-# font_manager.fontManager.addfont(r"D:\anaconda\Lib\site-packages\matplotlib\mpl-data\fonts\ttf\{}.ttf".format(myfont))
-# plt.rc('font',family='Alibaba PuHuiTi 2.0',color='#005493ff')
-# plt.rcParams['font.weight']='bold'
 plt.rcParams["axes.unicode_minus"]=False #该语句解决图像中的“-”负号的乱码问题
 plt.rcParams['axes.spines.right']=False###去掉图右边轴
 plt.rcParams['axes.spines.top']=False #####去掉图上轴
@@ -101,7 +96,7 @@ bnds = tuple((0,None) for x in x0)
 cons = ({'type': 'eq', 'fun': total_weight_constraint})
 #cons = ({'type':'eq', 'fun': lambda x: sum(x) - 1})
 options={'disp':False, 'maxiter':1000, 'ftol':1e-20}
-solution = minimize(risk_budget_objective, x0,args=(cov), bounds=bnds, constraints=cons, method='SLSQP', options=options)
+solution = scipy.optimize.minimize(risk_budget_objective, x0,args=(cov), bounds=bnds, constraints=cons, method='SLSQP', options=options)
 final_weights=solution.x
 for i in range(len(final_weights)):
     st.write(f'{final_weights[i]:.1%}投资于{R_cov.columns[i]}')
